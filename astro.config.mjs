562dfc456443219383b9ac3bacf0cce03b4f30df
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-01-31 16:51:17 -0700

    initial docs added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [
+    tailwind(),
+    starlight(
+      {
+        title: 'Adventure Time API',
+        logo:{
+          src: './src/assets/logo.svg',
+          alt: 'Astro logo',
+          replacesTitle: true,
+        },
+        social: {
+          discord: 'https://astro.build/chat',
+          github: 'https://github.com/withastro/starlight',
+        },
+        customCss:['./src/styles/custom.css']
+        
+      },
+      mdx()
+    ),
+  ],
 });
---
+++ src/content/docs/docs/index.md
@@ -0,0 +1,11 @@
+---
+title: Welcome
+
+sidebar:
+  # Set a custom label for the link
+ name: Welcome
+  # Set a custom order for the link (lower numbers are displayed higher up
+
+---
+
+Welcome to Adventure Time Docs
